ddc24af0f20b39ccc15fe2332b70a0a3c940f460
ccm36 <cameron.c.malloy@gmail.com>
2019-05-27 18:37:01 -0500

    Set up Navbar and routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import '../styles/App.css'; 
 import Navbar from './utils/Navbar'
+import Users from './users/Users'
+import NewTask from './todos/NewTask'
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Route exact path="/" render={() => <h3>Home Screen</h3>} />
+      <Route path="/team-members" component={Users} />
+      <Route path="/new-task" container={NewTask} />
     </div>
   );
 }
