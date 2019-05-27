2ed683a26a82bd5727862ddd70a46712f9573e66
ccm36 <cameron.c.malloy@gmail.com>
2019-05-27 18:28:13 -0500

    Add Navbar to screen and begin setting up router

--- src/components/utils/Navbar.js
+++ src/components/utils/Navbar.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../../styles/Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div> 
+        <nav className="navbar">
+            <NavLink exact to="/" activeClassName="active">OM ToDo</NavLink>
+            <NavLink to ="/team-members" activeClassName="active">Team Members</NavLink>
+            <NavLink to ="/new-task" activeClassName="active">Add Task</NavLink>
+        </nav> 
     )
 }
 
